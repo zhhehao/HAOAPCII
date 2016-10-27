eb600124ed1c3468ffc70b35021a267ae540a869
zhhehao <zh.hehao@gmail.com>
2016-10-27 20:44:21 +0800

    ac

--- ch4/exer/uva253.cpp
+++ ch4/exer/uva253.cpp
@@ -1,5 +1,5 @@
 /*
-** use 2 functions, rotatex,, rotatey to simulate
+** use 3 functions, rotateX, rotateY, rotateZ to simulate
 */
 
 # include <cstdio>
@@ -8,91 +8,77 @@ using namespace std;
 
 # define MAXN 6
 
-char cube_s[MAXN], cube_t[MAXN], cube_c[MAXN];
+char cube_s[MAXN], cube_c[MAXN];
 
-void rotateX() {
+int cmpCube() {
+	int ok = 1;
+	for (int i = 0; i < 6; i++)
+		if (cube_c[i] != cube_s[i]) {
+			ok = 0;
+			break;
+		}
+	return ok;
+}
+
+int rotateX() {
 	char t = cube_c[1];
 	cube_c[1] = cube_c[2];
 	cube_c[2] = cube_c[4];
 	cube_c[4] = cube_c[3];
 	cube_c[3] = t;
+	return cmpCube();
 }
 
-void rotateY() {
+int rotateY() {
 	char t = cube_c[1];
 	cube_c[1] = cube_c[5];
 	cube_c[5] = cube_c[4];
 	cube_c[4] = cube_c[0];
 	cube_c[0] = t;
+	return cmpCube();
 }
 
-void getcube1() {
-	strcpy(cube_c, cube_s);
-}
-
-void getcube2() {
-	strcpy(cube_c, cube_s);
-}
-
-void getcube3() {
-	strcpy(cube_c, cube_s);
-}
-
-void getcube4() {
-	strcpy(cube_c, cube_s);
-}
-
-void getcube5() {
-	strcpy(cube_c, cube_s);
-}
-
-void getcube6() {
-	strcpy(cube_c, cube_s);
-}
-
-int cmpCube() {
-	int cnt = 4;
-	while(cnt--) {
-		rotateX();
-		if (strcmp(cube_c, cube_t) == 0)
-			return 1;
-	}
-	return 0;
-}
-
-int checkCube() {
-	getcube1();
-	if(cmpCube())
-		return 1;
-	getcube2();
-	if(cmpCube())
-		return 1;
-	getcube3();
-	if(cmpCube())
-		return 1;
-	getcube4();
-	if(cmpCube())
-		return 1;
-	getcube5();
-	if(cmpCube())
-		return 1;
-	getcube6();
-	if(cmpCube())
-		return 1;
-	return 0;
+int rotateZ() {
+	char t = cube_c[0];
+	cube_c[0] = cube_c[2];
+	cube_c[2] = cube_c[5];
+	cube_c[5] = cube_c[3];
+	cube_c[3] = t;
+	return cmpCube();
 }
 
 int main(void) {
-	freopen("data.in", "r", stdin);
-	freopen("data.out", "w", stdout);
-	char buf[12];
+	// freopen("data.in", "r", stdin);
+	// freopen("data.out", "w", stdout);
+	char buf[15];
 	while(scanf("%s", buf) != EOF) {
 		for (int i = 0; i < 6; i++)
-			cube_s[i] = buf[i];
+			cube_c[i] = buf[i];
 		for (int i = 6; i < 12; i++)
-			cube_t[i-6] = buf[i];
+			cube_s[i-6] = buf[i];
 
-		if (checkCube())
+		bool is_same = false;
+		for (int i = 0; i < 4; i++) {
+			if (rotateX()) {
+				is_same = true;
+				goto end;
+			}
+			for (int j = 0; j < 4; j++) {
+				if (rotateY())  {
+					is_same = true;
+					goto end;
+				}
+				for (int k = 0; k < 4; k++) {
+					if (rotateZ()) {
+						is_same = true;
+						goto end;
+					}
+				}
+			}
+		}
+
+end:
+		if (is_same)
 			printf("TRUE\n");
 		else
 			printf("FALSE\n");
